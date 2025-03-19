966d0054fc11171521a78a4c8c046f417913cf88
julienNtb <julien.nottebaert1@gmail.com>
2025-03-19 14:51:12 +0100

    dyschool V1

--- app/dashboard/components/JeuxFavoris.jsx
+++ app/dashboard/components/JeuxFavoris.jsx
@@ -12,35 +12,32 @@ function JeuxFavoris () {
       <h3 className='text-secondary text-sm md:text-base font-bold'>
         Jeux favoris <FontAwesomeIcon icon={faHeart} className='ml-2' />
       </h3>
-      <div className='flex flex-wrap gap-4 md:gap-6 mt-4 md:mt-6 justify-center md:justify-between'>
+      <div className='flex flex-wrap gap-4 md:gap-4 mt-4 md:mt-6 justify-center md:justify-between'>
         <div className='flex flex-col justify-center'>
           <Image
             src={Jeu2}
-            className='w-36 h-20 md:w-44 md:h-24 lg:w-52 lg:h-28 opacity-30 hover:opacity-100 duration-100 cursor-pointer'
+            className='w-36 md:w-36  xl:w-52   hover:opacity-100 duration-100 cursor-pointer'
             style={{ objectFit: 'contain' }}
             alt='Jeux 1'
           />
-          <h4 className='text-center mt-2 text-xs md:text-sm'>Jeu des 7 familles</h4>
         </div>
 
         <div className='flex flex-col justify-center'>
           <Image
             src={Jeu1}
-            className='w-36 h-20 md:w-44 md:h-24 lg:w-52 lg:h-28 opacity-30 hover:opacity-100 duration-100 cursor-pointer'
+            className='w-36 md:w-36  xl:w-52   hover:opacity-100 duration-100 cursor-pointer'
             style={{ objectFit: 'contain' }}
             alt='Jeux 2'
           />
-          <h4 className='text-center mt-2 text-xs md:text-sm'>Mémory</h4>
         </div>
 
         <div className='flex flex-col justify-center'>
           <Image
             src={Jeu3}
-            className='w-36 h-20 md:w-44 md:h-24 lg:w-52 lg:h-28 opacity-30 hover:opacity-100 duration-100 cursor-pointer'
+            className='w-36 md:w-36  xl:w-52   hover:opacity-100 duration-100 cursor-pointer'
             style={{ objectFit: 'contain' }}
             alt='Jeux 3'
           />
-          <h4 className='text-center mt-2 text-xs md:text-sm'>Simon</h4>
         </div>
       </div>
     </div>
